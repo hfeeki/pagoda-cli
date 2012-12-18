ef9509080ca4cf579f18f8f857c42f1b4b13e659
lyon <lyondhill@gmail.com>
2012-12-17 17:20:24 -0700

    let danila try it

--- lib/pagoda/cli/helpers/tunnel.rb
+++ lib/pagoda/cli/helpers/tunnel.rb
@@ -4,42 +4,37 @@ module Pagoda::Command
   class Tunnel < Base
 
     def run
-      if (api.app_show(app)[:service_type] == :family)
-        # api tunnel commands
-      else
-        user_input = options[:component] || args.first
-        puts user_input
-        component = {}
-        begin
-          if user_input =~ /^(web\d*)|(db\d*)|(cache\d*)|(worker\d*)$/
-            puts "getting list"
-            components = client.component_list(app)
-            puts components
-            components.delete_if {|x| x[:cuid] != user_input }
-            puts 'filtering components'
-            component = components[0]
-            puts component
-          else
-            component = client.component_info(app, user_input)
-            puts component
-          end
-        rescue Exception => e
-          puts e.message
-          puts e.backtrace
-          output_error
-        end
-        output_error unless component
-        if component[:tunnelable]
-          type = component[:_type]
-          component_id = component[:_id]
-          app_id = component[:app_id]
-          Pagoda::Tunnel.new(type, user, password, app_id, component_id).start
+      user_input = options[:component] || args.first
+      puts user_input
+      component = {}
+      begin
+        if user_input =~ /^(web\d*)|(db\d*)|(cache\d*)|(worker\d*)$/
+          puts "getting list"
+          components = client.component_list(app)
+          puts components
+          components.delete_if {|x| x[:cuid] != user_input }
+          puts 'filtering components'
+          component = components[0]
+          puts component
         else
-          error "Either the component is not tunnelable or you do not have access"
+          component = client.component_info(app, user_input)
+          puts component
         end
-        
+      rescue Exception => e
+        puts e.message
+        puts e.backtrace
+        output_error
       end
-
+      output_error unless component
+      if component[:tunnelable]
+        type = component[:_type]
+        component_id = component[:_id]
+        app_id = component[:app_id]
+        Pagoda::Tunnel.new(type, user, password, app_id, component_id).start
+      else
+        error "Either the component is not tunnelable or you do not have access"
+      end
+      
     end
 
     def output_error
